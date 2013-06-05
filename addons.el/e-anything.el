9c0c7156f63b2531d642218eb6a1bb6c3f1b6cce
chenzp <chenzp@pset.suntec.net>
2013-06-05 16:00:29 +0800

    add anything mode

---
+++ addons.el/e-anything.el
@@ -0,0 +1,17 @@
+(add-to-list 'load-path "~/.emacs.d/elpa/anything-1.287")
+(add-to-list 'load-path "~/.emacs.d/elpa/anything-complete-1.86")
+(add-to-list 'load-path "~/.emacs.d/elpa/anything-config-0.4.1")
+(add-to-list 'load-path "~/.emacs.d/elpa/anything-match-plugin-1.27")
+(add-to-list 'load-path "~/.emacs.d/elpa/anything-obsolete-0.1")
+
+(eval-after-load 'anything
+  '(progn
+     (setq anything-enable-digit-shortcuts t)
+     (global-set-key (kbd "<f9>") 'anything)))
+
+(eval-after-load 'anything-config
+  '(add-to-list 'anything-sources anything-c-source-file-cache))
+
+(require 'anything)
+(require 'anything-config)
+(require 'anything-complete)
