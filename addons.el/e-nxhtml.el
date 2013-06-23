fe713a2ef198009d79f6dacc63748979eca24800
C Daniel <thatways.c@gmail.com>
2013-06-23 20:51:18 +0800

    update nxhtml config

--- addons.el/e-nxhtml.el
+++ addons.el/e-nxhtml.el
@@ -1,6 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/addons.gt/nxhtml")
-;;(require 'web-autoload)
 (load "~/.emacs.d/addons.gt/nxhtml/autostart.el")
+
+(require 'web-autoload)
 
 (when (and (= emacs-major-version 24)
            (>= emacs-minor-version 2))
@@ -9,7 +10,7 @@
            (delq 'buffer-file-name mumamo-per-buffer-local-vars))))
 
 ;;(setq mumamo-background-colors nil)
-(setq nxml-child-indent 2)  ;; work convention is 4 spaces for HTML, etc
-;; (setq nxml-outline-child-indent 4)
-;; (setq mumamo-submode-indent-offset 4)
-(setq sgml-basic-offset 2)
+(setq nxml-child-indent 4)  ;; work convention is 4 spaces for HTML, etc
+(setq nxml-outline-child-indent 4)
+(setq mumamo-submode-indent-offset 4)
+(setq sgml-basic-offset 4)
