2c59e5fb53672994d55f4738f064bd013b02d466
chenzp <chenzp@pset.suntec.net>
2013-06-24 10:39:28 +0800

    remove nxhtml autostart.el since error prompted on windows

--- addons.el/e-nxhtml.el
+++ addons.el/e-nxhtml.el
@@ -1,13 +1,14 @@
 (add-to-list 'load-path "~/.emacs.d/addons.gt/nxhtml")
 (load "~/.emacs.d/addons.gt/nxhtml/autostart.el")
 
-(require 'web-autoload)
+;;(require 'web-autoload)
 
 (when (and (= emacs-major-version 24)
            (>= emacs-minor-version 2))
   (eval-after-load "mumamo"
     '(setq mumamo-per-buffer-local-vars
            (delq 'buffer-file-name mumamo-per-buffer-local-vars))))
+
 
 ;;(setq mumamo-background-colors nil)
 (setq nxml-child-indent 4)  ;; work convention is 4 spaces for HTML, etc
