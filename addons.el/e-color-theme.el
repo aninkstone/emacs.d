61db08135a6e9163e1cfbd3a9518adb0e742d66e
chenzp <chenzp@pset.suntec.net>
2013-07-05 10:57:43 +0800

    Different emacs24 deftheme for emacsclient -t/-c frames

--- addons.el/e-color-theme.el
+++ addons.el/e-color-theme.el
@@ -1,5 +1,17 @@
 ;;color theme setting
 (add-to-list 'load-path "~/.emacs.d/addons.st/color-theme-6.6.0")
 (add-to-list 'load-path "~/.emacs.d/addons.gt/color-theme-tangotango")
+
 (require 'color-theme-tangotango)
-(color-theme-tangotango)
+;;(color-theme-tangotango)
+
+(defun mb/pick-color-theme (frame)
+  (select-frame frame)
+  (if (window-system frame)
+      (progn  
+        (color-theme-tangotango))))
+
+(add-hook 'after-make-frame-functions 'mb/pick-color-theme)
+
+;; For when started with emacs or emacs -nw rather than emacs --daemon
+(if window-system  (color-theme-tangotango))
