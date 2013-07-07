62ed27425d08c092ec6f65d8441db8f51749f723
C Daniel <thatways.c@gmail.com>
2013-07-07 17:23:34 +0800

    fix bug on mac

--- default.el
+++ default.el
@@ -61,7 +61,7 @@
   ;;(setq-default tab-width nil)
   (global-auto-revert-mode nil)))
 
-(if (eq system-type 'darwin)
+(if (eq system-type "darwin")
     ;;append brew path
     (setq PATH (format "%s:%s" "/usr/local/bin" (getenv "PATH")))
     (setq exec-path (append exec-path '("/usr/local/bin"))))
