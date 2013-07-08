46924ca198f3b2baa87fd2a51410ae56b196fe38
parallels <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-07-08 21:37:40 +0800

    support multi on linux

--- addons.el/e-color-theme.el
+++ addons.el/e-color-theme.el
@@ -3,24 +3,34 @@
 (add-to-list 'load-path "~/.emacs.d/addons.gt/color-theme-tangotango")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/addons.gt/color-theme-tangotango")
 
+
+;;(if (eq system-type 'gnu/linux)
+;;  (progn
+;;    (load-theme 'tangotango)
+;;    (enable-theme 'tangotango)
+;;    (defun mb/pick-color-theme (frame)
+;;      (select-frame frame)
+;;      (if (window-system frame) (enable-theme 'tangotango) (disable-theme 'tangotango)))
+;;    (add-hook 'after-make-frame-functions 'mb/pick-color-theme)
+;;
+;;    (if window-system (enable-theme 'tangotango)(disable-theme 'tangotango)))
+;;  (progn
+;;    (require 'color-theme-tangotango)
+;;    (defun mb/pick-color-theme (frame)
+;;      (select-frame frame)
+;;      (if (window-system frame) (color-theme-tangotango))))
+;;    (add-hook 'after-make-frame-functions 'mb/pick-color-theme)
+;;
+;;    (if window-system (color-theme-tangotango))
+;;)
+
+(require 'color-theme-tangotango)
+
 (set-variable 'color-theme-is-global nil)
 
-(if (eq system-type 'gnu/linux)
-  (progn
-    (load-theme 'tangotango)
-    (enable-theme 'tangotango)
-    (defun mb/pick-color-theme (frame)
-      (select-frame frame)
-      (if (window-system frame) (enable-theme 'tangotango) (disable-theme 'tangotango)))
-    (add-hook 'after-make-frame-functions 'mb/pick-color-theme)
+(defun mb/pick-color-theme (frame)
+  (select-frame frame)
+  (if (window-system frame) (color-theme-tangotango) (tool-bar-mode -1)))
+(add-hook 'after-make-frame-functions 'mb/pick-color-theme)
 
-    (if window-system (enable-theme 'tangotango)(disable-theme 'tangotango)))
-  (progn
-    (require 'color-theme-tangotango)
-    (defun mb/pick-color-theme (frame)
-      (select-frame frame)
-      (if (window-system frame) (color-theme-tangotango))))
-    (add-hook 'after-make-frame-functions 'mb/pick-color-theme)
-
-    (if window-system (color-theme-tangotango))
-)
+(if window-system (color-theme-tangotango))
