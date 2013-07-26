6c6bd837682b3310d0bf9aaa9cfdcd0c383c1fd8
chenzp <chenzp@pset.suntec.net>
2013-07-26 17:19:29 +0800

    disable color-theme on win32

--- addons.el/e-color-theme.el
+++ addons.el/e-color-theme.el
@@ -1,18 +1,48 @@
 ;;color theme setting
-(add-to-list 'load-path "~/.emacs.d/addons.st/color-theme-6.6.0")
-(add-to-list 'load-path "~/.emacs.d/addons.gt/color-theme-tangotango")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/addons.gt/color-theme-tangotango")
+;;(add-to-list 'load-path "~/.emacs.d/addons.st/color-theme-6.6.0")
+;;(add-to-list 'load-path "~/.emacs.d/addons.gt/color-theme-tangotango")
+;;(add-to-list 'custom-theme-load-path "~/.emacs.d/addons.gt/color-theme-tangotango")
+;;
+;;(require 'color-theme-tangotango)
+;;(if window-system (color-theme-tangotango))
+;;
+;;(set-variable 'color-theme-is-global nil)
+;;
+;;(defun mb/pick-color-theme (frame)
+;;  (select-frame frame)
+;;  (if (window-system frame) 
+;;      (progn (color-theme-tangotango)
+;;             (tool-bar-mode -1)
+;;             (set-frame-parameter nil 'fullscreen 'fullboth))))
+;;
+;;(add-hook 'after-make-frame-functions 'mb/pick-color-theme)
 
-(require 'color-theme-tangotango)
-(if window-system (color-theme-tangotango))
+;;color theme setting
+(if (eq system-type 'windows-nt)
 
-(set-variable 'color-theme-is-global nil)
+  (progn 
+    (defun mb/pick-color-theme (frame)
+    (select-frame frame)
+    (if (window-system frame)
+        (progn (tool-bar-mode -1)
+               (set-frame-parameter nil 'fullscreen 'fullboth))))
+    (add-hook 'after-make-frame-functions 'mb/pick-color-theme))
 
-(defun mb/pick-color-theme (frame)
-  (select-frame frame)
-  (if (window-system frame) 
-      (progn (color-theme-tangotango)
-             (tool-bar-mode -1)
-             (set-frame-parameter nil 'fullscreen 'fullboth))))
+  (progn 
+    (add-to-list 'load-path "~/.emacs.d/addons.st/color-theme-6.6.0")
+    (add-to-list 'load-path "~/.emacs.d/addons.gt/color-theme-tangotango")
+    (add-to-list 'custom-theme-load-path "~/.emacs.d/addons.gt/color-theme-tangotango")
+    (require 'color-theme-tangotango)
+    (if window-system (color-theme-tangotango))
 
-(add-hook 'after-make-frame-functions 'mb/pick-color-theme)
+    (set-variable 'color-theme-is-global nil)
+
+    (defun mb/pick-color-theme (frame)
+      (select-frame frame)
+      (if (window-system frame)
+          (progn 
+            (color-theme-tangotango)
+            (tool-bar-mode -1)
+            (set-frame-parameter nil 'fullscreen 'fullboth))))
+
+  (add-hook 'after-make-frame-functions 'mb/pick-color-theme)))
