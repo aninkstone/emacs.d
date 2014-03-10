a32adfd36dc1fbb7a1e31bc4916dca8d08813e75
chenzp <chenzp@pset.suntec.net>
2014-03-10 09:54:46 +0800

    update e-org.el

--- addons.el/e-org.el
+++ addons.el/e-org.el
@@ -52,10 +52,9 @@
 (setq org-directory "~/Documents/orgs")
 (setq org-default-notes-file "~/Documents/orgs/note.org")
 (setq org-agenda-files (list "~/Documents/orgs/jobs.org"
+                             "~/Documents/orgs/meeting.org"
                              "~/Documents/orgs/refile.org"))
 
-;; I use C-M-r to start capture mode
-(global-set-key (kbd "C-M-r") 'org-capture)
 ;; I use C-c r to start capture mode when using SSH from my Android phone
 (global-set-key (kbd "C-c r") 'org-capture)
 
@@ -67,14 +66,8 @@
                "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
               ("n" "note" entry (file "~/Documents/orgs/refile.org")
                "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-              ("j" "Journal" entry (file+datetree "~/Documents/orgs/diary.org")
-               "* %?\n%U\n" :clock-in t :clock-resume t)
-              ("w" "org-protocol" entry (file "~/Documents/orgs/refile.org")
-               "* TODO Review %c\n%U\n" :immediate-finish t)
-              ("p" "Phone call" entry (file "~/Documents/orgs/refile.org")
-               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
-              ("h" "Habit" entry (file "~/Documents/orgs/refile.org")
-               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"<%Y-%m-%d %a .+1d/3d>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+              ("m" "meeting" entry (file "~/Documents/orgs/meeting.org") 
+               "* TODO %?\n%U\n:START:\n:END:\n"))))
 
 ; Targets include this file and any file contributing to the agenda - up to 9 levels deep
 (setq org-refile-targets (quote ((nil :maxlevel . 9) (org-agenda-files :maxlevel . 9))))
