c013e50c92350ae315a6949a1eded51288dad0b3
Daniel <thatways.c@gmail.com>
2014-12-14 14:18:26 +0800

    disable speedbar mode default. since start error on emacs 24.4

--- default.el
+++ default.el
@@ -19,7 +19,7 @@
 (load-file "~/.emacs.d/addons.el/e-magit.el")
 (load-file "~/.emacs.d/addons.el/e-mcomplete.el")
 (load-file "~/.emacs.d/addons.el/e-shell.el")
-(load-file "~/.emacs.d/addons.el/e-sr-speedbar.el")
+;;(load-file "~/.emacs.d/addons.el/e-sr-speedbar.el")
 (load-file "~/.emacs.d/addons.el/e-undo-tree.el")
 (load-file "~/.emacs.d/addons.el/e-package.el")
 (load-file "~/.emacs.d/addons.el/e-ahs.el")
